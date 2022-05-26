af846f0b7554d78413a2652f0a9ee871495876c2
SmokingRat <halfbrikbarry@gmail.com>
2022-05-26 10:50:02 +0300

    Добавьте файлы проекта.

---
+++ Task.Module15/Classroom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Module15
+{
+    /// <summary>
+    /// Class of classrooms
+    /// </summary>
+    class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Task.Module15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task.Module15
+{
+    /// <summary>
+    /// Entry class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Entry method
+        /// </summary>
+        /// <param name="args">Entry parameter</param>
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+
+            var allStudents = GetAllStudents(classes);
+            Console.WriteLine(string.Join(" ", allStudents));
+            Console.ReadLine();
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var fin = from stu in classes
+                      from a in stu.Students
+                      select a;
+            return fin.ToArray();
+        }
+    }
+}
